d256415b222ef6b3209af2999f1fb62143d019a0
VinayakFia <vinayak.fialok@outlook.com>
2023-04-02 22:20:10 +1000

    location

--- src/pages/MapView.js
+++ src/pages/MapView.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, LoadScript, InfoWindowF } from "@react-google-maps/api";
 import { API_KEY } from "../config";
 
 const MapView = () => {
+  const [location, setLocation] = useState(null);
+
   const center = {
     lat: -33.9173,
     lng: 151.2313,
   };
 
-  const location = {
-    lat: -33.9172,
-    lng: 151.2313,
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      console.log("FUCK");
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
   };
 
   return (
@@ -20,14 +34,15 @@ const MapView = () => {
         zoom={17}
         mapContainerStyle={{ height: "100vh", width: "100vw" }}
       >
-        <InfoWindowF key={0} position={location}>
-          <div>
-            Hello from Christian
-          </div>
-        </InfoWindowF>
+        {" "}
+        {location != null ? (
+          <InfoWindowF key={0} position={location}>
+            <div>Hello from Christian</div>
+          </InfoWindowF>
+        ) : null}
       </GoogleMap>
     </LoadScript>
   );
-}
+};
 
 export default MapView;
