5ca19ec0deac712661d00d4c86e36d3f306a8ae4
Edward Lukman <edwlaw12@gmail.com>
2023-05-23 23:06:48 +1000

    adding mapview to homepage

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import MapView from './MapView';
 
 const HomePage = () => {
     return (
+      <div>
         <nav className="navbar">
-        <h1 className="logo">Who's On Campus?</h1>
-        <ul className="nav-links">
-          <li><a href="\">Friend List</a></li>
-          <li><a href="\">Map</a></li>
-          <li><a href="\">Stats</a></li>
-          <li><a href="\">Profile</a></li>
-          <li><a href="/login">Logout</a></li>
-        </ul>
-      </nav>
+            <h1 className="logo">Who's On Campus?</h1>
+            <ul className="nav-links">
+              <li><a href="\">Profile</a></li>
+              <li><a href="/login">Logout</a></li>
+            </ul>
+          </nav>
+          <div className='home-container'>
+            <div className="map-container">
+              <MapView/>
+            </div>
+            <div className='location-form'>
+              {/* yet to be done */}
+            </div>
+          </div>
+          <footer className="footer">
+        <p>&copy; 2023 CSESoc-Trainee 4 | Visit our repository <a href="https://github.com/csesoc/trainee-4-23t1">here</a></p>
+      </footer>
+      </div>
+      
     );
 };
 
